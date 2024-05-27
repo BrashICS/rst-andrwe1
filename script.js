cebd35d6deecfa7bd5b590de9551b474b5b00384
andrwe1 <andrew.leech@stu.ocsb.ca>
2024-05-27 13:29:15 -0400

    oirrvjs

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 /**
  * ICS4U - Final Project
- * 
+ *
  * Description:
- * 
- * Author:
+ *
+ * Author: Andrew Leech
  */
 
 'use strict';
+
+
+let c = document.getElementById('myCanvas');
+let canvas = c.getContext("2d");
+
+// canvas.moveTo(0, 0);
+// canvas.lineTo(200, 0);
+// canvas.stroke();
+
+ canvas.fillStyle = "#000";
+
+for(let i = 0; i != 20; i++) {
+for (let x = 0; x != 8; x++) {
+    canvas.strokeRect(x * 500/ 8 ,i * 750/20,500/8, 500/8)
+  }
+}
