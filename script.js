347b8f9d1e284bd719318a529523c52523b30f1d
andrwe1 <andrew.leech@stu.ocsb.ca>
2024-05-27 20:22:45 -0400

    rweg

--- script.js
+++ script.js
@@ -9,17 +9,28 @@
 'use strict';
 
 
-let c = document.getElementById('myCanvas');
-let canvas = c.getContext("2d");
+// let c = document.getElementById('myCanvas');
+// let canvas = c.getContext("2d");
 
-// canvas.moveTo(0, 0);
-// canvas.lineTo(200, 0);
-// canvas.stroke();
+// // canvas.moveTo(0, 0);
+// // canvas.lineTo(200, 0);
+// // canvas.stroke();
 
- canvas.fillStyle = "#000";
+//  canvas.fillStyle = "#000";
 
-for(let i = 0; i != 20; i++) {
-for (let x = 0; x != 8; x++) {
-    canvas.strokeRect(x * 500/ 8 ,i * 750/20,500/8, 500/8)
-  }
+// for(let i = 0; i != 20; i++) {
+// for (let x = 0; x != 8; x++) {
+//     canvas.strokeRect(x * 500/ 8 ,i * 750/20,500/8, 500/8)
+//   }
+// }
+
+function setup() {
+  createCanvas(100, 100, 'myCanvas');
+
+  background(200);
+
+  // Draw a diagonal line.
+  line(0, 0, 10, 10);
+
+  describe('A diagonal line drawn from top-left to bottom');
 }
