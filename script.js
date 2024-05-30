1c1063c9d1f01084f471edb9909571d1cecce83a
Andrwe1 <andrew.leech@stu.ocsb.ca>
2024-05-30 09:52:08 -0400

    KOWABUNGA

--- script.js
+++ script.js
@@ -11,29 +11,38 @@
 // ----------VARS (Not the place)-------------------------------------------------------------------------
 
 let cvs;
+let cvs_x = 400;
+let cvs_y = 800;
 let grid = [];
 
 // colours
-let grey = [128, 128, 128];
-let black = [0, 0, 0];
+let grey = [245, 245, 245];
+let black = [235, 235, 235];
 
 // ----------Classes--------------------------------------------------------------------------------------
 
 class Square {
   occupied = false;
-  colour;
+  colour = [255, 0, 0];
 
   constructor(colour) {
     this.colour = colour;
   }
 }
 
+class Shapes {
+  colour;
+}
 
+class cube extends Shapes {
+
+}
 
 // ----------Functions -----------------------------------------------------------------------------------
 
 function setup() {
-  cvs = createCanvas(1000, 2000);
+  cvs = createCanvas(cvs_x, cvs_y);
+   background(0)
 
   // grid array
   for(let y = 0; y < 20; y++) {
@@ -46,8 +55,25 @@ function setup() {
       }
     }
   }
-
-  // HTML Grid
-  
+  grid[0][0].colour = [255, 0, 0]
 }
+
+function draw() {
+  background(220);
+
+  draw_grid(cvs_x, cvs_y);
+}
+
+function draw_grid(cvs_x, cvs_y) {
+  stroke("black");
+  for (let row = 0; row < 20; row++) {
+    for (let col = 0; col < 10; col++) {
+
+      fill(grid[row][col].colour);
+      rect(col* (cvs_x / 10), row * (cvs_y / 20), 100, 100);
+
+    }
+  }
+}
+
 
