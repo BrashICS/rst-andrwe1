4245595699f2db4031fa7cf08ab35781d77b8156
andrwe1 <andrew.leech@stu.ocsb.ca>
2024-06-13 23:44:12 -0400

    This isn't gonna be finsihed on time

--- Script.js
+++ Script.js
@@ -11,20 +11,15 @@
 
 document.addEventListener('keydown', event => {if (event.key.toLowerCase() == 'w') current_peice.rotate()});
 document.addEventListener('keydown', event => {if (event.key.toLowerCase() == 'a') current_peice.left()});
-document.addEventListener('keydown', event => {if (event.key.toLowerCase() == 's') move1()});
+document.addEventListener('keydown', event => {if (event.key.toLowerCase() == 's') current_peice.down()});
 document.addEventListener('keydown', event => {if (event.key.toLowerCase() == 'd') current_peice.right()});
 document.addEventListener('keydown', event => {if (event.key == ' ') rever()});
-document.getElementById('play').addEventListener('click', new_piece);
+document.getElementById('play').addEventListener('click', new_game);
 document.getElementById('pause').addEventListener('click', pause);
 
 let points = document.getElementById('score');
 
 // ----------VARS (Not the place)-------------------------------------------------------------------------
-function rever() {
-  let arr = [1,2]
-  arr.reverse()
-  console.log(arr)
-}
 let cvs;
 let cvs_x = 400;
 let cvs_y = 800;
@@ -32,7 +27,7 @@ let grid = [];
 let peices = []
 let current_peice;
 let temp;
-let gameover = false;
+let gameover;
 let score = 0;
 let blank;
 
@@ -49,6 +44,7 @@ let brown = [165, 42, 42];
 // ----------Classes--------------------------------------------------------------------------------------
 
 class Square {
+  // Each square on the grid gets it's own object, in grid 2d array
   occupied = false;
   colour;
   original;
@@ -59,6 +55,7 @@ class Square {
   }
 
   changeColour(colour) {
+    // Changes colour on grid
     this.colour = colour;
   }
 
@@ -67,17 +64,18 @@ class Square {
     this.occupied = false;
   }
 
-  drop(colour, occupied) {
-    if(occupied) {
-      this.restore();
-    } else {
-      this.colour = colour;
-      this.occupied == true;
-    }
-  }
+  // drop(colour, occupied) {
+  //   if(occupied) {
+  //     this.restore();
+  //   } else {
+  //     this.colour = colour;
+  //     this.occupied == true;
+  //   }
+  // }
 }
 
 class Shapes {
+  // Class for each shape
   colour;
   bottom = false;
   sq1;
@@ -87,6 +85,7 @@ class Shapes {
   rotation = 0;
 
   appear() {
+    // Changes the colour of the grid for each piece of the shape
     grid[this.sq1[0]][this.sq1[1]].changeColour(this.colour);
     grid[this.sq2[0]][this.sq2[1]].changeColour(this.colour);
     grid[this.sq3[0]][this.sq3[1]].changeColour(this.colour);
@@ -94,6 +93,7 @@ class Shapes {
   }
 
   dissapear() {
+    // returns each piece their normal state
     grid[this.sq1[0]][this.sq1[1]].restore();
     grid[this.sq2[0]][this.sq2[1]].restore();
     grid[this.sq3[0]][this.sq3[1]].restore();
@@ -101,6 +101,9 @@ class Shapes {
   }
 
   down() {
+    // Moves the x value of each piece down by 1, unless it does not meet the perameters
+
+    //Check if the piece under it is occupied
     if (grid[this.sq1[0] + 1][this.sq1[1]].occupied == true) {
       this.place();
       return;
@@ -119,25 +122,27 @@ class Shapes {
     }
 
 
-    grid[this.sq1[0]][this.sq1[1]].changeColour(grid[this.sq1[0]][this.sq1[1]].original);
-    grid[this.sq2[0]][this.sq2[1]].changeColour(grid[this.sq2[0]][this.sq2[1]].original);
-    grid[this.sq3[0]][this.sq3[1]].changeColour(grid[this.sq3[0]][this.sq3[1]].original);
-    grid[this.sq4[0]][this.sq4[1]].changeColour(grid[this.sq4[0]][this.sq4[1]].original);
+    this.dissapear();
 
+    // Move down by 1
     this.sq1[0] += 1;
     this.sq2[0] += 1;
     this.sq3[0] += 1;
     this.sq4[0] += 1;
 
-  this.appear();
+   this.appear();
 
+   // If it has hit the bottom, stop
   if (this.sq1[0] == 19 || this.sq2[0] == 19 || this.sq3[0] == 19 || this.sq4[0] == 19 ) this.place();
   }
 
   left() {
+    //Moves x pieces left, as long as parameters are true
 
-    if(this.sq1[1] == 0 || this.sq1[1] == 0) return;
+    // If the piece is on the far left
+    if(this.sq1[1] == 0) return;
 
+    // Checks if the piece under it is occupied
     else if (grid[this.sq1[0]][this.sq1[1] - 1].occupied == true) {
       return;
     }
@@ -151,21 +156,21 @@ class Shapes {
       return;
     }
 
-    grid[this.sq1[0]][this.sq1[1]].changeColour(grid[this.sq1[0]][this.sq1[1]].original);
-    grid[this.sq2[0]][this.sq2[1]].changeColour(grid[this.sq2[0]][this.sq2[1]].original);
-    grid[this.sq3[0]][this.sq3[1]].changeColour(grid[this.sq3[0]][this.sq3[1]].original);
-    grid[this.sq4[0]][this.sq4[1]].changeColour(grid[this.sq4[0]][this.sq4[1]].original);
+    this.dissapear();
 
     this.sq1[1] -= 1;
     this.sq2[1] -= 1;
     this.sq3[1] -= 1;
     this.sq4[1] -= 1;
+
     this.appear();
   }
 
   right() {
-    if(this.sq4[1] == 9 || this.sq4[0] == 19) return;
+    // Checks if the piece is on the far right
+    if(this.sq4[1] == 9) return;
 
+    // Checks if the piece under it is occupied
     else if (grid[this.sq1[0]][this.sq1[1] + 1].occupied == true) {
       return;
     }
@@ -178,25 +183,33 @@ class Shapes {
     else if (grid[this.sq4[0]][this.sq4[1] + 1].occupied == true) {
       return;
     }
-    grid[this.sq1[0]][this.sq1[1]].changeColour(grid[this.sq1[0]][this.sq1[1]].original);
-    grid[this.sq2[0]][this.sq2[1]].changeColour(grid[this.sq2[0]][this.sq2[1]].original);
-    grid[this.sq3[0]][this.sq3[1]].changeColour(grid[this.sq3[0]][this.sq3[1]].original);
-    grid[this.sq4[0]][this.sq4[1]].changeColour(grid[this.sq4[0]][this.sq4[1]].original);
+
+    this.dissapear();
 
     this.sq1[1] += 1;
     this.sq2[1] += 1;
     this.sq3[1] += 1;
     this.sq4[1] += 1;
+
     this.appear();
   }
 
    rotate() {
 
+    // Cubes don't rotate
     if (this.type == 'cube') return;
 
     this.dissapear();
 
+    // Creates new array of each square coordinate
     let blocks = [this.sq1, this.sq2, this.sq3, this.sq4];
+
+    // let past_blocks = new Array(4)
+    // for(let i = 0; i != 4; i++) {
+    //   past_blocks.push(blocks[i]);
+    // }
+
+    // Relocates each piece relative to sq2 being (0,0) on a cartesian plane
     let x_buffer = this.sq2[1];
     let y_buffer = this.sq2[0];
 
@@ -205,30 +218,35 @@ class Shapes {
       blocks[i][1] -= x_buffer;
     }
 
+    // To rotate a point on a grid 90 degrees, switch x and y and change the sign on the y-coordinate
     for (let x = 0; x != 4; x++) {
       blocks[x].reverse();
       blocks[x][0] *= - 1;
     }
 
+    // Remove the buffer to rotated points back on the grid
     for (let i = 0; i != 4; i++) {
       blocks[i][0] += y_buffer;
       blocks[i][1] += x_buffer;
     }
+
     this.appear();
   }
 
   place() {
-
+    // Give each peice a permenant spot on the grid
     grid[this.sq1[0]][this.sq1[1]].occupied = true;
     grid[this.sq2[0]][this.sq2[1]].occupied = true;
     grid[this.sq3[0]][this.sq3[1]].occupied = true;
     grid[this.sq4[0]][this.sq4[1]].occupied = true;
 
     this.bottom = true;
+
     this.appear();
 
     line_check();
 
+    // Points for each piece placed
     score += 10;
     points.innerText = 'Score: ' + score;
   }
@@ -302,20 +320,12 @@ class Z extends Shapes {
 
 // ------ Canvas Stuff ------------------------------
 function setup() {
+  // Creates canvas
   cvs = createCanvas(cvs_x, cvs_y);
    background(0)
 
   // grid array
-  for(let y = 0; y < 20; y++) {
-    grid[y] = [];
-    for (let x = 0; x < 10; x++) {
-      if ((x + y) % 2 == 0) {
-        grid[y].push(new Square(grey));
-      } else {
-        grid[y].push(new Square(black));
-      }
-    }
-  }
+  new_grid();
 }
 
 function draw() {
@@ -326,6 +336,7 @@ function draw() {
 
 function draw_grid(cvs_x, cvs_y) {
   stroke("black");
+  // Creates array
   for (let row = 0; row < 20; row++) {
     for (let col = 0; col < 10; col++) {
 
@@ -338,10 +349,22 @@ function draw_grid(cvs_x, cvs_y) {
 
 // -- Other things ------------------
 
+function new_game() {
+  score = 0;
+
+  if(gameover == false) return;
+  gameover = false;
+
+  // Resetting
+  new_grid();
+  score = 0;
+  new_piece();
+}
+
+
 function move1() {
+  // Drops each piece by one square after certain time delay
   if(current_peice.bottom == true) {
-    console.log('working')
-    // setTimeout(timeSaver, 40)
     new_piece();
     return;
   }
@@ -351,14 +374,14 @@ function move1() {
 }
 
 function move2() {
+  // After each delay, peice moves down, restarts loop by calling move1()
   current_peice.down();
   move1();
 }
 
 function new_piece() {
 
-
-  if (gameover == true) return;
+  // if top of the grid is occupied gameover is true
   for(let i = 0; i != 3; i++) {
     if (grid[0][3 + i].occupied == true) gameover = true;;
   }
@@ -366,6 +389,7 @@ function new_piece() {
   if (gameover == true) return;
 
 
+  // Randomly chooses next piece
   let choice = randInt(0, 6);
   if (choice == 0) current_peice = new Bar();
   if (choice == 1) current_peice = new Cube();
@@ -381,10 +405,13 @@ function new_piece() {
 
 function line_check() {
 
-  let bryan = [new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey)];
+  let num_lines = 0;
+  // Two empty lines (RIP Bryan and Brett)
+  let empty_line1 = [new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey)];
 
-  let brett = [new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black)];
+  let empty_line2 = [new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black), new Square(grey), new Square(black)];
 
+  // Checks if any lines are formed
   for (let i = 0; i != 20; i++) {
     let line = true;
     for (let x = 0; x != 10; x++) {
@@ -392,14 +419,27 @@ function line_check() {
         line = false;
        }
       }
+      // Removes line from grid and adds new one to the top
       if (line == true) {
+          num_lines++;
           grid.splice(i, 1)
           if (grid[0][0].colour == black) {
-          grid.unshift(brett);
+          grid.unshift(empty_line2);
           } else {
-            grid.unshift(bryan);
+            grid.unshift(empty_line1);
           }
         }
+      }
+
+      // Points for consecutive ammounts of lines
+      if(num_lines == 1) {
+        score += 100;
+      } else if(num_lines == 2) {
+        score += 300;
+      } else if(num_lines == 3) {
+        score += 500;
+      } else if (num_lines == 4) {
+        score += 800;
       }
     }
 
@@ -423,4 +463,17 @@ function randInt(min, max) {
 function pause() {
   gameover = true;
   current_peice.bottom = true;
+}
+
+function new_grid() {
+  for(let y = 0; y < 20; y++) {
+    grid[y] = [];
+    for (let x = 0; x < 10; x++) {
+      if ((x + y) % 2 == 0) {
+        grid[y].push(new Square(grey));
+      } else {
+        grid[y].push(new Square(black));
+      }
+    }
+  }
 }
